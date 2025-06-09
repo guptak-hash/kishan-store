b2f55ce2ccc482e357df43afd290238db30175bb
guptak-hash <guptakishan1123@gmail.com>
2025-06-09 20:57:01 +0530

    checked

--- src/App.jsx
+++ src/App.jsx
@@ -11,47 +11,6 @@ import Navbar from './components/Shared/Navbar';
 import { useSelector } from 'react-redux';
 
 
-// function App() {
-//   const theme = useSelector((state) => state.theme.mode);
-
-//   return (
-//     <BrowserRouter>
-//       <Navbar />
-//       <Routes>
-//          {/* Public routes */}
-//         <Route path="/" element={<HomePage />} />
-//         <Route path="/products" element={<ProductsPage />} />
-//         <Route path="/login" element={<LoginPage />} />
-//         <Route path="/register" element={<RegisterPage />} />
-        
-//         {/* Protected routes */}
-//         <Route
-//           path="/products/:id"
-//           element={
-//             <ProtectedRoute>
-//               <ProductDetailsPage />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/cart"
-//           element={
-//             <ProtectedRoute>
-//               <CartPage />
-//             </ProtectedRoute>
-//           }
-//         />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-
-
-
-
-
-
 
 function App() {
   const theme = useSelector((state) => state.theme.mode);
